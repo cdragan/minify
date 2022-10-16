c35bbe14160240c67f7c22dd1e72c0f9496aa7f0
Chris Dragan <chris@dragan.dev>
2022-10-16 14:44:26 +0100

    add test for high entropy data

--- test_arith_encode.c
+++ test_arith_encode.c
@@ -118,7 +118,7 @@ int main(void)
 
     {
         const uint8_t input[3]  = { 0xFF, 0xFF, 0x00 };
-        uint8_t       output[3] = { 0xAA, 0xAA };
+        uint8_t       output[3] = { 0xAA, 0xAA, 0xAA };
         const size_t  out_size  = arith_encode(&output, 3, &input, 3, 256);
         TEST(out_size == 3);
         TEST(output[0] == 0xF0);
@@ -128,7 +128,7 @@ int main(void)
 
     {
         const uint8_t input[3]  = { 0xFF, 0xFF, 0x00 };
-        uint8_t       output[3] = { 0xAA, 0xAA };
+        uint8_t       output[3] = { 0xAA, 0xAA, 0xAA };
         const size_t  out_size  = arith_encode(&output, 2, &input, 3, 8);
         TEST(out_size == 2);
         TEST(output[0] == 0xF3);
@@ -137,13 +137,41 @@ int main(void)
 
     {
         const uint8_t input[3]  = { 0x55, 0xAA, 0x55 };
-        uint8_t       output[4] = { 0xAA, 0xAA };
+        uint8_t       output[4] = { 0xAA, 0xAA, 0xAA, 0xAA };
         const size_t  out_size  = arith_encode(&output, 4, &input, 3, 64);
         TEST(out_size == 4);
         TEST(output[0] == 0x9A);
         TEST(output[1] == 0xA8);
         TEST(output[2] == 0x61);
         TEST(output[3] == 0x80);
+    }
+
+    /* Encode high entropy data */
+    {
+        const uint8_t input[16]  = { 0xB2, 0x3D, 0x55, 0x0D, 0xCC, 0x4B, 0x63, 0x04,
+                                     0x0B, 0xCD, 0xE2, 0x68, 0x9C, 0xFE, 0xCC, 0x2B };
+        uint8_t       output[20] = { 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA,
+                                     0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA,
+                                     0xAA, 0xAA, 0xAA, 0xAA };
+        const size_t  out_size  = arith_encode(&output, 20, &input, 16, 128);
+        TEST(out_size == 17);
+        TEST(output[0]  == 0x61);
+        TEST(output[1]  == 0x19);
+        TEST(output[2]  == 0xB8);
+        TEST(output[3]  == 0x3A);
+        TEST(output[4]  == 0x62);
+        TEST(output[5]  == 0x80);
+        TEST(output[6]  == 0x34);
+        TEST(output[7]  == 0xA2);
+        TEST(output[8]  == 0x41);
+        TEST(output[9]  == 0x41);
+        TEST(output[10] == 0xCB);
+        TEST(output[11] == 0xC1);
+        TEST(output[12] == 0xDD);
+        TEST(output[13] == 0x81);
+        TEST(output[14] == 0x4C);
+        TEST(output[15] == 0x98);
+        TEST(output[16] == 0x8F);
     }
 
     return num_failed ? EXIT_FAILURE : EXIT_SUCCESS;
